2ff6fac8c2ccf992706c5ec52728317aee10868a
tam <shaked9724@pm.me>
2024-05-26 18:43:26 +0100

    Changes:
    1. Added minor print before loading languages (as it may take a long time)
    2. Setup gunicorn in Dockerfile using entrypoint.sh
    3. Minor changes to support 3.11 and 3.12

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
+if [ "$LT_PORT" = "" ]; then
+  LT_PORT=5000
+fi
 
-gunicorn --bind 0.0.0.0:"$LT_PORT" 'wsgi:app'
+gunicorn \
+  --chdir /app/libretranslate \
+  --timeout 900 \
+  --bind 0.0.0.0:"$LT_PORT" 'wsgi:app'
