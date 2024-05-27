ada99787172dd6f7c95576c40626cafd393a4844
tam <shaked9724@pm.me>
2024-05-27 17:32:52 +0100

    Dockerfile will use Gunicorn

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+if [ "$LT_PORT" = "" ]; then
+  LT_PORT=5000
+fi
+
+./venv/bin/gunicorn \
+  --chdir /app/libretranslate \
+  --timeout 900 \
+   --pythonpath /app \
+  --bind 0.0.0.0:"$LT_PORT" 'wsgi:app'
